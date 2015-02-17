0aab5c20e0b638cbe1cb68eaf2641093218ebfaf
Vivek Kumar <vivekk.chugh@gmail.com>
2015-02-16 20:19:01 -0800

    update sever method

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-// http://stackoverflow.com/questions/6084360/using-node-js-as-a-simple-web-server
-var connect = require('connect');
-var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen(process.env.PORT || 8000);
+
+ var express = require("express");
+ var app = express();
+
+// Routing all other requests
+app.use(express.static(__dirname + '/public'));
+
+
+  
+var port = process.env.PORT || 5000;
+    app.listen(port, function() { 
+        console.log("Listening on " + port); 
+    });
+
